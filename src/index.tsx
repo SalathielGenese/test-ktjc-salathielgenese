fb2eab4051d0df4cf3aa49e39df7c70f7a6c334b
SalathielGenese <salathielgenese@gmail.com>
2019-06-26 14:57:46 +0100

    Initialize @ionic/pwa-elements

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,8 @@
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+defineCustomElements( window );
