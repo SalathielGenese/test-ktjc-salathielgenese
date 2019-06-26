0477705c379b4dc6627d7dbdfeda69bc33aa97c2
SalathielGenese <salathielgenese@gmail.com>
2019-06-26 15:07:58 +0100

    FIX indentation to 4 spaces

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,17 +2,16 @@ import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/react';
 import React from 'react';
 
 const Home: React.SFC<any> = () => {
-  return (
-    <>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Closest pharmacy</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent padding>
-      </IonContent>
+    return <>
+        <IonHeader>
+            <IonToolbar>
+                <IonTitle>Closest pharmacy</IonTitle>
+            </IonToolbar>
+        </IonHeader>
+        <IonContent padding>
+        </IonContent>
     </>
-  );
+    ;
 };
 
 export default Home;
