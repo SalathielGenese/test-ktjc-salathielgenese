410231a0e7412ce49215ed38b2a318bd40aeb79b
SalathielGenese <salathielgenese@gmail.com>
2019-06-26 07:33:51 +0100

    Capture form value changes

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,5 +1,5 @@
 import { LoginPresentation } from './login-presentation';
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, FormEvent, useEffect } from 'react';
 
 
 
@@ -10,6 +10,34 @@ export const Login = () => {
     const formRef = useRef<HTMLFormElement>( null );
     const usernameRef = useRef<HTMLIonInputElement>( null );
     const passwordRef = useRef<HTMLIonInputElement>( null );
+
+    useEffect( () => {
+        const current = usernameRef.current;
+
+        if ( !current ) return;
+
+        const inputListener = ( event: FormEvent<HTMLIonInputElement> ) =>
+            setUsername( event.currentTarget.value! )
+        ;
+
+        current.addEventListener( 'input', inputListener as never );
+
+        return () => current.removeEventListener( 'input', inputListener as never );
+    }, [ usernameRef ]);
+
+    useEffect( () => {
+        const current = passwordRef.current;
+
+        if ( !current ) return;
+
+        const inputListener = ( event: FormEvent<HTMLIonInputElement> ) =>
+            setUsername( event.currentTarget.value! )
+        ;
+
+        current.addEventListener( 'input', inputListener as never );
+
+        return () => current.removeEventListener( 'input', inputListener as never );
+    }, [ passwordRef ]);
 
     return <LoginPresentation
         ref={ formRef }
