bebbc96142e537b0a0a44a6dda17375f8ff168e3
SalathielGenese <salathielgenese@gmail.com>
2019-06-26 07:25:43 +0100

    Introduce state value for password and username

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,9 +1,12 @@
 import { LoginPresentation } from './login-presentation';
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 
 
 
 export const Login = () => {
+    const [ username, setUsername ] = useState( '' );
+    const [ password, setPassword ] = useState( '' );
+
     const formRef = useRef<HTMLFormElement>( null );
     const usernameRef = useRef<HTMLIonInputElement>( null );
     const passwordRef = useRef<HTMLIonInputElement>( null );
